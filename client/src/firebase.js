e27d69993b91b5e75b3fb3b026fcf706284a0753
Saurav1928 <sauravfarkade9191@gmail.com>
2024-06-19 17:44:33 +0530

    Google auth fixed

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,12 +6,13 @@ import { initializeApp } from 'firebase/app';
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-estate.firebaseapp.com',
-  projectId: 'mern-estate',
-  storageBucket: 'mern-estate.appspot.com',
-  messagingSenderId: '1078482850952',
-  appId: '1:1078482850952:web:28f19139ab77246602fb3d',
-};
+  authDomain: "mern-estate-96053.firebaseapp.com",
+  projectId: "mern-estate-96053",
+  storageBucket: "mern-estate-96053.appspot.com",
+  messagingSenderId: "173468735836",
+  appId: "1:173468735836:web:2d29f18b4fe47c6da34999",
+  measurementId: "G-W7NYYQCKF5",
+}
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
